4c842db3638d2648d2bb5658b11a296db528fe18
Gwhite564 <89479335+Gwhite564@users.noreply.github.com>
2021-08-30 18:18:44 -0500

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -12,6 +12,8 @@
 
   printf("Cameron White, Grant White!!\n");
   printf("Major is Computer Science\n");
-
+  printf("Grant White\n");
+  printf("Major is Electrical Engineering\n");
+    
   return 0;
 }
